ad59efc2895790a4b39c95a6f10d4589be12efd2
yuyang chen <yuyangchen@yuyangs-MBP-2.home>
2019-01-27 20:56:45 -0500

    update DFS and BFS with java code

---
+++ BFS/BFS with parent node.py
@@ -0,0 +1,30 @@
+graph = {
+	"A": ["B","C"],
+	"B": ["A","C","D"],
+	"C": ["A","B","D","E"],
+	"D": ["B","C","E","F"],
+	"E": ["C","D"],
+	"F": ["D"]
+}
+
+def BFS(graph, starting):
+	queue = []
+	queue.append(starting)
+	seen = set()
+	seen.add(starting)
+	parent = {starting: None}
+
+	while (len(queue) > 0):
+		vertex = queue.pop(0)
+		neighbor_nodes = graph[vertex]
+		for node in neighbor_nodes:
+			if node not in seen:
+				queue.append(node)
+				seen.add(node)
+				parent[node] = vertex
+		print(vertex)
+	return parent
+
+parent = BFS(graph, "A")
+for key in parent:
+	print(key, parent[key])
